c6d7c2d8faaa05d973f5fed2a4925957e3839609
Artur Lacerda <artur.aml@gmail.com>
2018-04-15 13:25:01 -0300

    Move FishDatset and UNet to src folder

---
+++ src/FishDataset.py
@@ -0,0 +1,84 @@
+
+import os
+import re
+import numpy as np
+import random
+import tarfile
+import urllib
+from PIL import Image
+from glob import glob
+import matplotlib.pyplot as plt
+from torch.utils.data.dataset import Dataset
+
+class FishDataset(Dataset):
+    """Fishes dataset."""
+
+    def __init__(self, root_dir, transform=None, target_transform=None, download=False):
+        """
+        Args:
+            root_dir (string): Data directory containing the fish_image and mask_image folders.
+            transform (callable, optional): Optional transform to be applied on an image.
+        """
+        self.root_dir = os.path.abspath(root_dir)
+        self.transform = transform
+        self.target_transform = target_transform
+
+        if download:
+            self.download()
+
+        if not self._check_exists():
+            raise RuntimeError('Dataset not found. You can use download=True to download it.')
+
+
+        self.images = glob(os.path.join(root_dir, 'fish_image/*/*.png'))
+        self.masks = [re.sub('fish', 'mask', image) for image in self.images]
+        self.labels = [int(re.search('.*fish_image/fish_(\d+)', image).group(1)) for image in self.images]
+
+    def __len__(self):
+        return len(self.labels)
+
+    def __getitem__(self, index):
+        label = self.labels[index]
+        image = Image.open(self.images[index])
+        mask = Image.open(self.masks[index])
+
+        if mask.mode == '1':
+            mask = mask.convert('L')
+
+        # https://github.com/pytorch/vision/issues/9
+        seed = np.random.randint(2147483647)
+        random.seed(seed)
+        if self.transform is not None:
+            image = self.transform(image)
+
+        random.seed(seed)
+        if self.target_transform is not None:
+            mask = self.target_transform(mask)
+            mask = mask.round()
+
+        return (image, mask, label)
+
+
+    def download(self):
+        if self._check_exists():
+            return
+
+        try:
+            os.makedirs(self.root_dir)
+        except FileExistsError:
+            pass
+
+        url = 'http://groups.inf.ed.ac.uk/f4k/GROUNDTRUTH/RECOG/Archive/fishRecognition_GT.tar'
+        file_path = os.path.join(self.root_dir, 'fishRecognition_GT.tar')
+        print('Downloading...', end=' ')
+        urllib.request.urlretrieve(url, file_path)
+        print('Done!')
+        print('Extracting files...', end=' ')
+        with tarfile.open(file_path) as tar:
+                tar.extractall(self.root_dir)
+        os.remove(file_path)
+        print('Done!')
+
+    def _check_exists(self):
+        return os.path.exists(os.path.join(self.root_dir, 'fish_image')) and \
+               os.path.exists(os.path.join(self.root_dir, 'mask_image'))
---
+++ src/model.py
@@ -0,0 +1,74 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+
+class conv_block(nn.Module):
+    def __init__(self, in_channels,  out_channels):
+        super().__init__()
+        self.conv = nn.Conv2d(in_channels, out_channels, 3, padding=1)
+        self.batch_norm = nn.BatchNorm2d(out_channels)
+
+    def forward(self, x):
+        x = self.conv(x)
+        x = self.batch_norm(x)
+        return F.relu(x)
+
+class UNet(nn.Module):
+    def __init__(self):
+        super().__init__()
+        self.down1 = nn.Sequential(
+            conv_block(3, 16),
+            conv_block(16, 16)
+        )
+        self.down2 = nn.Sequential(
+            conv_block(16, 32),
+            conv_block(32, 32)
+        )
+        self.down3 = nn.Sequential(
+            conv_block(32, 64),
+            conv_block(64, 64)
+        )
+
+        self.middle = conv_block(64, 64)
+
+        self.up3 = nn.Sequential(
+            conv_block(128, 128),
+            conv_block(128, 32)
+        )
+
+        self.up2 = nn.Sequential(
+            conv_block(64, 64),
+            conv_block(64, 16)
+        )
+
+        self.up1 = nn.Sequential(
+            conv_block(32, 32),
+            conv_block(32, 1)
+        )
+
+    def forward(self,  x):
+        down1 = self.down1(x)
+        out = F.max_pool2d(down1, 2)
+
+        down2 = self.down2(out)
+        out = F.max_pool2d(down2, 2)
+
+        down3 = self.down3(out)
+        out = F.max_pool2d(down3, 2)
+
+        out = self.middle(out)
+
+        out = F.upsample(out, scale_factor=2)
+        out = torch.cat([down3, out], 1)
+        out = self.up3(out)
+
+        out = F.upsample(out, scale_factor=2)
+        out = torch.cat([down2, out], 1)
+        out = self.up2(out)
+
+        out = F.upsample(out, scale_factor=2)
+        out = torch.cat([down1, out], 1)
+        out = self.up1(out)
+        out = F.sigmoid(out)
+
+        return out
