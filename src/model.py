abe523b1715931bdb26750c0904808b9210ad176
Ubuntu <ubuntu@ip-172-31-25-212.us-west-2.compute.internal>
2018-04-21 17:30:46 +0000

    Train model

--- src/model.py
+++ src/model.py
@@ -1,11 +1,14 @@
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
+import torch.nn.init as init
+import numpy as np
 
 class conv_block(nn.Module):
     def __init__(self, in_channels,  out_channels):
         super().__init__()
         self.conv = nn.Conv2d(in_channels, out_channels, 3, padding=1)
+        init.xavier_uniform(self.conv.weight, gain=np.sqrt(2))
         self.batch_norm = nn.BatchNorm2d(out_channels)
         self.leaky_relu = nn.LeakyReLU(0.01)
 
@@ -30,17 +33,8 @@ class UNet(nn.Module):
             conv_block(64, 128),
             conv_block(128, 128)
         )
-        self.down4 = nn.Sequential(
-            conv_block(128, 256),
-            conv_block(256, 256)
-        )
 
-        self.middle = conv_block(256,256)
-
-        self.up4 = nn.Sequential(
-            conv_block(512, 512),
-            conv_block(512, 128)
-        )
+        self.middle = conv_block(128, 128)
 
         self.up3 = nn.Sequential(
             conv_block(256, 256),
@@ -51,6 +45,7 @@ class UNet(nn.Module):
             conv_block(128, 128),
             conv_block(128, 32)
         )
+
         self.up1 = nn.Sequential(
             conv_block(64, 64),
             conv_block(64, 1)
@@ -66,14 +61,7 @@ class UNet(nn.Module):
         down3 = self.down3(out)
         out = F.max_pool2d(down3, 2)
 
-        down4 = self.down4(out)
-        out = F.max_pool2d(down4, 2)
-
         out = self.middle(out)
-
-        out = F.upsample(out, scale_factor=2)
-        out = torch.cat([down4, out], 1)
-        out = self.up4(out)
 
         out = F.upsample(out, scale_factor=2)
         out = torch.cat([down3, out], 1)
